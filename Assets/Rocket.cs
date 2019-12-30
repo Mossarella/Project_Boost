8791e8704d906dcb5616e93176efa5567f567613
Pomelowy <mosno6@gmail.com>
2019-12-31 03:58:34 +0700

    movement?

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    Rigidbody rigidBody;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -16,18 +17,21 @@ public class Rocket : MonoBehaviour
         ProcessInput();
     }
 
-    private static void ProcessInput()
+    private void ProcessInput()
     {
         if (Input.GetKey(KeyCode.Space))
-            {
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
             print("Thrusting");                
-            }
+        }
         if (Input.GetKey(KeyCode.A))
-            {
+        {
+            transform.Rotate(Vector3.forward);
             print("Rotating left");
-            }
+        }
         else if (Input.GetKey(KeyCode.D))
         {
+            transform.Rotate(Vector3.back);
             print("Rotating right");
         }
 
