a7a9f15621999a0fd501df4c9794420666d62b23
Pomelowy <mosno6@gmail.com>
2020-01-01 00:50:03 +0700

    added sound to rocket

---
+++ Assets/Scenes/Rocket.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rigidBody;
+    AudioSource audiosource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        audiosource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+            print("Thrusting");
+            audiosource.Play();
+        }
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            audiosource.Stop();
+        }
+
+        
+        Rotating();
+
+    }
+
+    void Rotating()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward);
+            print("Rotating left");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.back);
+            print("Rotating right");
+        }
+    }
+}
