3c49d2871049b68162ae822f4ebf16ff19e72931
Pomelowy <mosno6@gmail.com>
2020-01-03 03:57:52 +0700

    invoked

--- Assets/Scenes/Rocket.cs
+++ Assets/Scenes/Rocket.cs
@@ -1,11 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
-{
+{ enum State { Alive, Dying, Transcend }
     Rigidbody rigidBody;
     AudioSource audiosource;
+    [SerializeField] float rThrust = 100f;
+    [SerializeField] float accelerateThrust = 100f;
+
+    State state = State.Alive;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,17 +22,54 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Rotating();
-        Thrust();
-        Sound();
+        if (state == State.Alive)
+        {
+            Rotating();
+            Thrust();
+            Sound();
+        }
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (state != State.Alive)
+        {
+            return;
+        }
+        
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":           
+            break;
+            case "Finish":
+                state = State.Transcend;
+                Invoke("LoadNextLevel", 1f); //parameter this "time"
+                break;
+            default:
+                state = State.Dying;
+                print("Ded");
+                Invoke("LoadFirstLevel", 1f);
+                break;
+        }
+        
+    }
+
+    void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    void LoadNextLevel()
+    {
+        SceneManager.LoadScene(1);
     }
 
     void Thrust()
     {
+        float thrustThisFrame = accelerateThrust * Time.deltaTime;
+
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidBody.AddRelativeForce(Vector3.up);
-            print("Thrusting");
+            rigidBody.AddRelativeForce(Vector3.up *thrustThisFrame);
         }           
     }
 
@@ -44,15 +87,20 @@ public class Rocket : MonoBehaviour
 
     void Rotating()
     {
+        rigidBody.freezeRotation = true; // pause physics on press
+        
+        
+        float rotationThisFrame = rThrust * Time.deltaTime;
+        
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward);
-            print("Rotating left");
+            transform.Rotate(Vector3.forward * rotationThisFrame);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(Vector3.back);
-            print("Rotating right");
+            transform.Rotate(Vector3.back * rotationThisFrame);
         }
+        rigidBody.freezeRotation = false; //resume physics when not pressed
+        
     }
 }
